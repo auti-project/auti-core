a86726e972c8a5469bb3d5871b640e53e50953d3
Tommy TIAN <xtianae@connect.ust.hk>
2023-01-10 15:57:20 +0800

    Sum check protocols for committees, add unit tests.
    Signed-off-by: txaty <xtianae@connect.ust.hk>

--- sumcheck/sumcheck.go
+++ sumcheck/sumcheck.go
@@ -8,31 +8,37 @@ import (
 	"github.com/auti-project/auti-core/transaction"
 )
 
-func CheckOrgEpoch(lastCommits [][]byte, currCommits [][]byte, txLists [][]*transaction.Hidden) (bool, error) {
+func CheckOrgEpoch(lastCommits, currCommits [][]byte, txLists [][]*transaction.Hidden) (
+	[]*edwards25519.Point, bool, error,
+) {
 	numLastCommits, numCurrCommits, numTXLists := len(lastCommits), len(currCommits), len(txLists)
 	if numLastCommits != numCurrCommits || numLastCommits != numTXLists {
-		return false, fmt.Errorf("number of last commits, current commits and transaction lists are not equal: %d, %d, %d",
-			numLastCommits, numCurrCommits, numTXLists)
+		return nil, false,
+			fmt.Errorf(
+				"number of last commits, current commits and transaction lists are not equal: %d, %d, %d",
+				numLastCommits, numCurrCommits, numTXLists)
 	}
+
 	if numLastCommits == 0 {
-		return false, fmt.Errorf("number of last commits, current commits and transaction lists are zero")
+		return nil, false, fmt.Errorf("number of last commits, current commits and transaction lists are zero")
 	}
+
 	commits := make([]*edwards25519.Point, numLastCommits)
 	for i := 0; i < numLastCommits; i++ {
-		commit, err := checkOrgOneTypeEpoch(lastCommits[i], currCommits[i], txLists[i])
+		commit, err := computeTXCommitCheck(lastCommits[i], currCommits[i], txLists[i])
 		if err != nil {
-			return false, err
+			return nil, false, err
 		}
 		commits[i] = commit
 	}
-	b := new(edwards25519.Point).Set(commits[0])
-	for i := 1; i < numLastCommits; i++ {
-		b.Add(b, commits[i])
+	check := edwards25519.NewIdentityPoint()
+	for _, commit := range commits {
+		check.Add(check, commit)
 	}
-	return b.Equal(edwards25519.NewIdentityPoint()) == 1, nil
+	return commits, check.Equal(edwards25519.NewIdentityPoint()) == 1, nil
 }
 
-func checkOrgOneTypeEpoch(lastCommit, currCommit []byte,
+func computeTXCommitCheck(lastCommit, currCommit []byte,
 	txList []*transaction.Hidden) (*edwards25519.Point, error) {
 	commit, err := new(edwards25519.Point).SetBytes(lastCommit)
 	if err != nil {
@@ -64,4 +70,78 @@ func checkOrgOneTypeEpoch(lastCommit, currCommit []byte,
 	}
 	commit.ScalarMult(randScalar, commit)
 	return commit, nil
+}
+
+func CheckAllOrgEpoch(orgLastCommits, orgEpochCommits, orgCurrCommits [][][]byte) (bool, error) {
+	var (
+		numOrgLastCommits  = len(orgLastCommits)
+		numOrgEpochCommits = len(orgEpochCommits)
+		numOrgCurrCommits  = len(orgCurrCommits)
+	)
+	if numOrgLastCommits != numOrgCurrCommits || numOrgLastCommits != numOrgEpochCommits {
+		return false,
+			fmt.Errorf(
+				"number of organizations is not consistent: %d, %d, %d",
+				numOrgLastCommits, numOrgCurrCommits, numOrgEpochCommits)
+	}
+
+	if numOrgLastCommits == 0 {
+		return false, fmt.Errorf("number of organizations is zero")
+	}
+
+	overallCheck := edwards25519.NewIdentityPoint()
+	for i := 0; i < len(orgLastCommits); i++ {
+		check, err := computeOrgCommitCheck(orgLastCommits[i], orgEpochCommits[i], orgCurrCommits[i])
+		if err != nil {
+			return false, err
+		}
+		overallCheck.Add(overallCheck, check)
+	}
+	return overallCheck.Equal(edwards25519.NewIdentityPoint()) == 1, nil
+}
+
+func computeOrgCommitCheck(lastCommits, epochCommits, currCommits [][]byte) (*edwards25519.Point, error) {
+	numLasts, numCurrents, numEpochs := len(lastCommits), len(currCommits), len(epochCommits)
+	if numLasts != numCurrents || numLasts != numEpochs {
+		return nil,
+			fmt.Errorf(
+				"number of last commits, current commits and epoch commits are not equal: %d, %d, %d",
+				numLasts, numCurrents, numEpochs)
+	}
+	if numLasts == 0 {
+		return nil, fmt.Errorf("number of last commits, current commits and epoch commits are zero")
+	}
+
+	var (
+		randBytes  = make([]byte, 64)
+		randScalar = new(edwards25519.Scalar)
+		orgCheck   = edwards25519.NewIdentityPoint()
+	)
+	for i := 0; i < numLasts; i++ {
+		check, err := new(edwards25519.Point).SetBytes(lastCommits[i])
+		if err != nil {
+			return nil, err
+		}
+		epochCommitPoint, err := new(edwards25519.Point).SetBytes(epochCommits[i])
+		if err != nil {
+			return nil, err
+		}
+		check.Add(check, epochCommitPoint)
+		currCommitPoint, err := new(edwards25519.Point).SetBytes(currCommits[i])
+		if err != nil {
+			return nil, err
+		}
+		check.Subtract(check, currCommitPoint)
+		_, err = rand.Read(randBytes)
+		if err != nil {
+			return nil, err
+		}
+		_, err = randScalar.SetUniformBytes(randBytes)
+		if err != nil {
+			return nil, err
+		}
+		check.ScalarMult(randScalar, check)
+		orgCheck.Add(orgCheck, check)
+	}
+	return orgCheck, nil
 }
--- sumcheck/sumcheck_test.go
+++ sumcheck/sumcheck_test.go
@@ -3,6 +3,7 @@ package sumcheck
 import (
 	"crypto/rand"
 	rand2 "math/rand"
+	"reflect"
 	"testing"
 	"time"
 
@@ -11,7 +12,30 @@ import (
 	"github.com/auti-project/auti-core/transaction"
 )
 
-func checkOrgOneTypeSetUp(numTXs int) (*edwards25519.Point, *edwards25519.Point, []*transaction.Hidden) {
+func computeTXCommitCheckSetUp(numTXs int) (*edwards25519.Point, *edwards25519.Point, []*transaction.Hidden) {
+	g, h := paramSetup()
+	lastCommit, err := commitment.Commit(12345, time.Now().UnixNano(), 0, g, h)
+	if err != nil {
+		panic(err)
+	}
+	lastCommitPoint, err := new(edwards25519.Point).SetBytes(lastCommit)
+	if err != nil {
+		panic(err)
+	}
+	currCommitPoint := new(edwards25519.Point).Set(lastCommitPoint)
+	txList := dummyTXs(g, h, numTXs)
+	tmp := new(edwards25519.Point)
+	for _, tx := range txList {
+		_, err = tmp.SetBytes(tx.Commitment)
+		if err != nil {
+			panic(err)
+		}
+		currCommitPoint.Add(currCommitPoint, tmp)
+	}
+	return lastCommitPoint, currCommitPoint, txList
+}
+
+func paramSetup() (*edwards25519.Point, *edwards25519.Point) {
 	randBytes := make([]byte, 64)
 	_, err := rand.Read(randBytes)
 	if err != nil {
@@ -33,25 +57,7 @@ func checkOrgOneTypeSetUp(numTXs int) (*edwards25519.Point, *edwards25519.Point,
 	}
 	h := edwards25519.NewGeneratorPoint()
 	h.ScalarMult(randScalar, h)
-	lastCommit, err := commitment.Commit(12345, time.Now().UnixNano(), 0, g, h)
-	if err != nil {
-		panic(err)
-	}
-	lastCommitPoint, err := new(edwards25519.Point).SetBytes(lastCommit)
-	if err != nil {
-		panic(err)
-	}
-	currCommitPoint := new(edwards25519.Point).Set(lastCommitPoint)
-	txList := dummyTXs(g, h, numTXs)
-	tmp := new(edwards25519.Point)
-	for _, tx := range txList {
-		_, err = tmp.SetBytes(tx.Commitment)
-		if err != nil {
-			panic(err)
-		}
-		currCommitPoint.Add(currCommitPoint, tmp)
-	}
-	return lastCommitPoint, currCommitPoint, txList
+	return g, h
 }
 
 func dummyTXs(g, h *edwards25519.Point, numTXs int) []*transaction.Hidden {
@@ -83,8 +89,8 @@ func dummyTXs(g, h *edwards25519.Point, numTXs int) []*transaction.Hidden {
 	return txList
 }
 
-func Test_checkOrgOneTypeEpoch(t *testing.T) {
-	lastCommit, currCommit, txList := checkOrgOneTypeSetUp(100)
+func Test_computeTXCommitCheck(t *testing.T) {
+	lastCommit, currCommit, txList := computeTXCommitCheckSetUp(100)
 	type args struct {
 		lastCommit []byte
 		currCommit []byte
@@ -97,7 +103,7 @@ func Test_checkOrgOneTypeEpoch(t *testing.T) {
 		wantErr bool
 	}{
 		{
-			name: "Test_checkOrgOneTypeEpoch",
+			name: "Test_computeTXCommitCheck",
 			args: args{
 				lastCommit: lastCommit.Bytes(),
 				currCommit: currCommit.Bytes(),
@@ -109,13 +115,140 @@ func Test_checkOrgOneTypeEpoch(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := checkOrgOneTypeEpoch(tt.args.lastCommit, tt.args.currCommit, tt.args.txList)
+			got, err := computeTXCommitCheck(tt.args.lastCommit, tt.args.currCommit, tt.args.txList)
 			if (err != nil) != tt.wantErr {
-				t.Errorf("checkOrgOneTypeEpoch() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("computeTXCommitCheck() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 			if got.Equal(tt.want) == 0 {
-				t.Errorf("checkOrgOneTypeEpoch() got = %v, want %v", got, tt.want)
+				t.Errorf("computeTXCommitCheck() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func checkAllOrgEpochSetup(numOrg, numTXs int) (orgLastCommits, orgEpochCommits, orgCurrCommits [][][]byte) {
+	orgLastCommits = make([][][]byte, numOrg)
+	orgEpochCommits = make([][][]byte, numOrg)
+	orgCurrCommits = make([][][]byte, numOrg)
+	var err error
+	for i := 0; i < numOrg; i++ {
+		orgLastCommits[i] = make([][]byte, 4)
+		orgEpochCommits[i] = make([][]byte, 4)
+		orgCurrCommits[i] = make([][]byte, 4)
+		for j := 0; j < 4; j++ {
+			g, h := paramSetup()
+			txList := dummyTXs(g, h, numTXs)
+			epochCommitPoint := edwards25519.NewIdentityPoint()
+			tmp := new(edwards25519.Point)
+			for _, tx := range txList {
+				_, err = tmp.SetBytes(tx.Commitment)
+				if err != nil {
+					panic(err)
+				}
+				epochCommitPoint.Add(epochCommitPoint, tmp)
+			}
+			lastCommit, err := commitment.Commit(12345, time.Now().UnixNano(), 0, g, h)
+			if err != nil {
+				panic(err)
+			}
+			lastCommitPoint, err := new(edwards25519.Point).SetBytes(lastCommit)
+			if err != nil {
+				panic(err)
+			}
+			currCommitPoint := new(edwards25519.Point).Add(epochCommitPoint, lastCommitPoint)
+			orgLastCommits[i][j] = lastCommit
+			orgEpochCommits[i][j] = epochCommitPoint.Bytes()
+			orgCurrCommits[i][j] = currCommitPoint.Bytes()
+		}
+	}
+	return orgLastCommits, orgEpochCommits, orgCurrCommits
+}
+
+func TestCheckAllOrgEpoch(t *testing.T) {
+	orgLastCommits, orgEpochCommits, orgCurrCommits := checkAllOrgEpochSetup(5, 100)
+	type args struct {
+		orgLastCommits  [][][]byte
+		orgEpochCommits [][][]byte
+		orgCurrCommits  [][][]byte
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    bool
+		wantErr bool
+	}{
+		{
+			name: "TestCheckAllOrgEpoch",
+			args: args{
+				orgLastCommits:  orgLastCommits,
+				orgEpochCommits: orgEpochCommits,
+				orgCurrCommits:  orgCurrCommits,
+			},
+			want:    true,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := CheckAllOrgEpoch(tt.args.orgLastCommits, tt.args.orgEpochCommits, tt.args.orgCurrCommits)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("CheckAllOrgEpoch() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("CheckAllOrgEpoch() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func checkOrgEpochSetup(numTXs int) (lastCommits, currCommits [][]byte, txLists [][]*transaction.Hidden) {
+	lastCommits = make([][]byte, 4)
+	currCommits = make([][]byte, 4)
+	txLists = make([][]*transaction.Hidden, 4)
+	for i := 0; i < 4; i++ {
+		var lastCommitPoint, currCommitPoint *edwards25519.Point
+		lastCommitPoint, currCommitPoint, txLists[i] = computeTXCommitCheckSetUp(numTXs)
+		lastCommits[i] = lastCommitPoint.Bytes()
+		currCommits[i] = currCommitPoint.Bytes()
+	}
+	return
+}
+
+func TestCheckOrgEpoch(t *testing.T) {
+	lastCommits, currCommits, txLists := checkOrgEpochSetup(100)
+	type args struct {
+		lastCommits [][]byte
+		currCommits [][]byte
+		txLists     [][]*transaction.Hidden
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    bool
+		wantErr bool
+	}{
+		{
+			name: "TestCheckOrgEpoch",
+			args: args{
+				lastCommits: lastCommits,
+				currCommits: currCommits,
+				txLists:     txLists,
+			},
+			want:    true,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, got, err := CheckOrgEpoch(tt.args.lastCommits, tt.args.currCommits, tt.args.txLists)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("CheckOrgEpoch() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("CheckOrgEpoch() got = %v, want %v", got, tt.want)
 			}
 		})
 	}
